80754fdb264b36f1ef05a72fccfc0947519ace6e
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-02-12 22:06:34 +0100

    feat: solve url shortening challenge in one go
    I should've done some commits, really forgot that because I was in flow.
    Shit. So I will at least rewrite the single steps that I undertook.
    
    1. install body parser, apply middleware to receive form input posts
    2. receive the url, shorten it (basically starting at 0 and counting up) and send json back to user
    3. have dynamic routs based on the shortened numbers, redirecting to the original url
    4. do backend url validation by creating URL(url) objects with javascripts own URL objects.
       additional filtering for http and https URL.protocl was necessary to pass the tests.
    
    DONE BABY! <3

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+
+// list of original urls and corresponding shortened urls
+let urlMappings = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// parse form field inputs
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// receive the url from the form field and shorten it
+app.post("/api/shorturl", (req, res) => {
+	//lets check the url for validity
+	if (isValidUrl(req.body.url) === true) {
+		const shortened = shortenURL(req.body.url);
+		res.json({
+			original_url: req.body.url,
+			short_url: shortened,
+		});
+		return;
+	}
+	res.json({
+		error: "invalid url",
+	});
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+	res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get("/api/shorturl/:shortenedNumber", (req, res) => {
+	const shortenedUrl = req.params.shortenedNumber;
+	const originalUrl = urlMappings[shortenedUrl];
+
+	if (originalUrl) {
+		res.redirect(originalUrl);
+		return;
+	}
+
+	res.status(404).json({ error: "URL not found" });
 });
+
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
+});
+
+// UTILITY FUNCTIONS
+
+//shortening
+function shortenURL(url) {
+	urlMappings.push(url);
+	console.table(urlMappings);
+	return urlMappings.indexOf(url);
+}
+
+//validity check
+function isValidUrl(url) {
+	try {
+		const newURL = new URL(url);
+		if (newURL.protocol != "https:" && newURL.protocol != "http:") {
+			return false;
+		}
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
